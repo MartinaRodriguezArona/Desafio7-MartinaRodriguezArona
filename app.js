b614944d010d9f3f4a5b067618f99314ba96c628
Martina Rodriguez Arona <tini.arona16@gmail.com>
2024-11-01 21:41:50 -1200

    Desafio7

---
+++ app.js
@@ -0,0 +1,9 @@
+let Preciocambiado = document.getElementById("precioProducto");
+
+console.log(Preciocambiado)
+
+Preciocambiado.innerText = "Precio: $450 ";
+
+const nombreProducto = document.querySelector("h1");
+
+nombreProducto.style.color = "violet"
